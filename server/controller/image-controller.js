f753964c0f8af94c0cb8707196c471e6a0f02b51
prabhat63s <prabhat6914@gmail.com>
2024-09-07 15:18:21 +0530

    server link change

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -12,7 +12,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `https://file-share-fe-theta.vercel.app/file/${file._id}`});
+        response.status(200).json({ path: `https://file-share-cn1a.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
