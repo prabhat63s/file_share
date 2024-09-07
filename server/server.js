7fab950baa9d92125d899075e4400e012f3c90bc
prabhat63s <prabhat6914@gmail.com>
2024-09-07 14:51:43 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes.js';
-import DBConnection from './database/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes.js";
+import DBConnection from "./database/db.js";
+import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const PORT = process.env.PORT || 8000;
+DBConnection();
+
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https:"],
+    credentials: true,
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
-const PORT = process.env.PORT || 8000;
-
-DBConnection();
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
